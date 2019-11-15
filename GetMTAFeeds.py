258e1912281d0b0cb803e432609129e18f476d2e
Tobias Bartsch <tobias-bartsch@gmx.net>
2019-11-14 21:18:44 -0500

    refactored code

--- GetMTAFeeds.py
+++ GetMTAFeeds.py
@@ -1,8 +1,13 @@
 import time
 import sys
-sys.path.append('/home/tbartsch/source/repos')
-from mtatracking.utils import utils
-from mtatracking.MTAdatamine import MTAdatamine
+#sys.path.append('/home/tbartsch/source/repos')
+#from mtatracking.utils import utils
+#from mtatracking.MTAdatamine import MTAdatamine
+from utils import utils
+from MTAdatamine import MTAdatamine
+
+#import mtatracking.nyct_subway_pb2 as nyct_subway_pb2
+#import mtatracking.gtfs_realtime_pb2 as gtfs_realtime_pb2
 #from MTAdatamodel import SubwaySystem
 
 #from MTAdatamodel import SubwaySystem
@@ -16,12 +21,14 @@ dmine = MTAdatamine(key)
 #Now let's track some trains
 delay = 10.0 #delay in seconds
 num = 2880 #how often do we track? 2880 = 8 hours for 10 s ticks
-feed_id = [1,26,16,21,2,11,31,36,51]
+#feed_id = [1,26,16,21,2,11,31,36,51]
+feed_id = ['gtfs-ace', 'gtfs-bdfm', 'gtfs-g', 'gtfs-jz', 'gtfs-nqrw', 'gtfs-l', 'gtfs', 'gtfs-7', 'gtfs-si']
 #MySubwaySys = SubwaySystem()
 
 starttime=time.time()
 
-while True:
+track = True
+while track:
    i = 0    
    while i < num:
        i+=1
@@ -34,12 +41,14 @@ while True:
        fname = 'tracking_results' + str(int(time.time())) + '.pkl'
        written = False
        while written is False:
-            try:
-                written = utils.write(tracking_results, fname)
-            except:
-                written = False
-                print("Writing to disk failed, retrying...")
-                pass
+            #try:
+            written = utils.write(tracking_results, fname)
+            print(written)
+            #except:
+            #    written = False
+            #    print("Writing to disk failed, retrying...")
+            #    track = False
+            #    break
        time.sleep(delay - ((time.time() - starttime) % delay)) #wait until we are supposed to sample the next set of data.
 
 #   fname = 'SubwaySys' + str(int(time.time())) + '.pkl'
